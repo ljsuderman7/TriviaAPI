7499e003593f052673f8c61b0a243c02917c8533
Leighton <ljsuderman7@gmail.com>
2022-09-19 16:10:12 -0400

    Initial commit

---
+++ app/src/main/java/ca/lsuderman/triviaapi/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.lsuderman.triviaapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
